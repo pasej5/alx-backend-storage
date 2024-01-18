9e6696e18f289279f67df85c02e821012944d719
Jealous Matsikachando <pasej5@gmail.com>
2024-01-18 15:36:12 +0200

    Quantity in the table items can be negative.

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,15 @@
+-- SQL script that creates a trigger that decreases
+-- the quantity of an item after adding a new order.
+-- Quantity in the table items can be negative.
+DROP TRIGGER IF EXISTS reduce_quantity;
+DELIMITER //
+CREATE TRIGGER after_order_insert
+AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+    UPDATE items
+    SET quantity = quantity - NEW.number
+    WHERE name = NEW.item_name;
+END;
+//
+DELIMITER ;
