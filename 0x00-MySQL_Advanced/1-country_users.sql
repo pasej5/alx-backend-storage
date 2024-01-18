af34f224b87c023813a3251f1383a3232829dcc0
Jealous Matsikachando <pasej5@gmail.com>
2024-01-18 03:21:00 +0000

    drop table first to avoid conficts

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,8 +1,9 @@
--- Script that creates a table users following these requirements:
--- id, email, name, country(enumeration of US, CO and TN)
-CREATE TABLE IF NOT EXISTS users(
-	id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	email varchar(255) NOT NULL UNIQUE,
-	name varchar(255),
-	country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
+-- SQL script that creates a table users following these requirements:
+-- id, email, name, country
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
 );
