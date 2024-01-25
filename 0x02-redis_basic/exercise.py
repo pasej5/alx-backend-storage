8e6f2bcaaecd02aaf73f2c100fab957f49544400
Jealous Matsikachando <pasej5@gmail.com>
2024-01-25 02:59:23 +0200

    Writing strings to Redis

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""Writing strings to Redis"""
+import redis
+import uuid
+
+
+class Cache:
+    """
+    Writing strings to Redis
+    """
+
+    def __init__(self):
+        """
+        Storing the instance of the redis cielnt
+        """
+        self._redis = redis.Redis()
+        self._redis.flushdb
+
+    def store(self, data) -> str:
+        """
+        Generate a random key
+
+        Args:
+            data: Input data that can be str, bytes, int, or float.
+
+        Returns:
+            str: The randomly generated key
+
+        """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
